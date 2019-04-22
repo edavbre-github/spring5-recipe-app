1dbe86bfd93ec936d2e0330dde070e1318a577d7
edavbre <david.brennan@ericsson.com>
2019-04-22 17:14:43 +0100

    Added some "cool" Thymeleaf formatting to the index page

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -2,9 +2,16 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 @SpringBootApplication
 public class Spring5RecipeAppApplication {
+
+    @Bean
+    public Java8TimeDialect java8TimeDialect() {
+        return new Java8TimeDialect();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5RecipeAppApplication.class, args);
