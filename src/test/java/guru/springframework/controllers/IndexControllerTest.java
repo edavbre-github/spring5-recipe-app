d9e57320aa6540ed4aee89504922d77cfff5e016
edavbre <david.brennan@ericsson.com>
2019-04-26 13:28:09 +0100

    Added more tests including an integration test. Added a YML file for CircleCI config.

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -1,14 +1,25 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Recipe;
 import guru.springframework.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
 
@@ -25,12 +36,41 @@ public class IndexControllerTest {
         indexController = new IndexController(recipeService);
     }
 
-
     @Test
     public void getIndexPage() {
+        // Given
+        Set<Recipe> recipes = new HashSet<>(2);
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipe.setDescription("lah dee dee dah");
+        recipe.setPrepTime(10);
+        recipe.setCookTime(30);
+        recipes.add(recipe);
+
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2L);
+        recipe2.setDescription("Dum dee dum dee dum");
+        recipe2.setPrepTime(15);
+        recipe2.setCookTime(25);
+        recipes.add(recipe2);
+
+        // When
+        when(recipeService.getRecipes()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
         String viewName = indexController.getIndexPage(model);
+
+        // Then
         assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
         verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+
+    @Test
+    public void testMockMvc() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 }
